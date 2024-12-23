b1579bd596a7303476647d46fd7bff1c4627fe53
Naza <sharon.doris54@gmail.com>
2024-12-23 23:27:24 +0100

    created the sig in, sign up and the forget password folders

--- food-delivery/App.js
+++ food-delivery/App.js
@@ -8,25 +8,41 @@ const slides = [
   {
     id: 1,
     title: 'Discover Best Places',
-    description: 'Find the best restaurants, cafes, and shops near you.',
-    image: require('./assets/images/onboarding 1.png'),
+    description: 'Discover the best places for food lovers! From cozy cafes to fine dining restaurants, explore top spots serving mouthwatering cuisines, local specialties, and unforgettable flavors.',
+    image: require('./assets/images/onboard.jpeg'),
   },
   {
     id: 2,
     title: 'Choose A Tasty Dish',
-    description: 'Find the best restaurants, cafes, and shops near you.',
-    image: require('./assets/images/onboarding 1.png'),
+    description: 'Choose a tasty dish that satisfies your cravings! From savory delights to sweet treats, explore a variety of flavors crafted to perfection.',
+    image: require('./assets/images/tasty.webp'),
   },
   {
     id: 3,
     title: 'Pick Up The Delivery',
-    description: 'Find the best restaurants, cafes, and shops near you.',
-    image: require('./assets/images/onboarding 1.png'),
+    description: 'Enjoy the convenience of pickup or delivery! Get your favorite dishes brought to your door or ready for you to grab on the go.',
+    image: require('./assets/images/onboarding 2.webp'),
   }
 ];
 
 export default function App() {
   const [showHomePage, setShowHomePage] = useState(false);
+
+  const buttonLabel = (label) =>{
+    return (
+      <View style={{
+        padding: 12,
+      }}>
+        <Text style={{
+          color: COLORS.title,
+          fontWeight: '600',
+          fontSize: SIZES.h4,
+        }}>
+          {label}
+        </Text>
+      </View>
+    )
+  }
 
   const onDone = () => {
     setShowHomePage(true);
@@ -42,25 +58,46 @@ export default function App() {
             <View style={{
               flex: 1,
               alignItems: 'center',
+              padding: 15,
+              paddingTop: 150,
             }}>
               <Image
                 source={item.image}
                 style={{
                   width: SIZES.width - 80,
                   height: 300,
+                  borderRadius: 25,
                 }} 
                 resizeMode="contain"// Example styling, adjust as needed
               />
-              <Text>
+              <Text style={{
+                fontWeight: 'bold',
+                color: COLORS.title,
+                fontSize: SIZES.h1,
+              }}>
                 {item.title}
               </Text>
-              <Text>
+              <Text style={{
+                textAlign: 'center',
+                paddingTop: 5,
+                color: COLORS.title,
+              }}>
                 {item.description}
               </Text>
             </View>
           );
         }}
-        onDone={onDone} // Call onDone when the intro is finished
+        activeDotStyle={{
+          backgroundColor: COLORS.primary,
+          width: 30,
+        }}
+        showSkipButton
+        renderNextButton={() => buttonLabel("Next")}
+        renderSkipButton={() => buttonLabel("Skip")}
+        renderDoneButton={() => buttonLabel("Done")}
+        onDone={() => {
+          setShowHomePage(true);
+        }}
       />
     );
   }
---
+++ food-delivery/screens/signin/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const SignInScreen = () => {
+    return (
+        <View>
+            <Text>Sign In Screen</Text>
+        </View>
+    );
+}
+
+export default SignInScreen;
